ef42aeab0c6892d72a49e11ee3715a72ded4cf9c
Shketik_R <ruslans3101@gmail.com>
2025-03-04 23:33:52 +0300

    product 2

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,9 +1,10 @@
 
-type Params = { id: string };
+type Params = Promise<{ id: string }>
 
 
-export default async function ProductPage(PageProps: { params: Params }) {
-  const { id } =  PageProps.params
+export default async function ProductPage( props : { params:  Params }) {
+  const params = await props.params
+  const id = params.id
   return (
     <div>ProductPage - {id}</div>
   )
